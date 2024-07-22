dbc4a5e6ff7996b59d1ae42abe5f5f1256a4e729
Nihed-Abd <nihed.abdennour@esprit.tn>
2024-07-22 11:24:17 +0100

    third commit

--- src/app/services/products.module.ts
+++ src/app/services/products.module.ts
@@ -2,9 +2,11 @@ export interface Product {
     id: number;
     name: string;
     price: number;
+    priceDiscount: number;
     description: string;
     image: string;
     category: string;
+    hasDiscount: boolean; // Add this property
 
   }
   
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -8,13 +8,13 @@ import { Product } from './products.module';
 export class ProductsService {
 
   private products: Product[] = [
-    { id: 1, name: 'Pizza Margherita', price: 8.99, description: 'Classic pizza with tomatoes and mozzarella', image: 'assets/images/pizza.jpg', category: 'foods' },
-    { id: 2, name: 'Spaghetti Carbonara', price: 10.99, description: 'Pasta with eggs, cheese, pancetta, and pepper', image: 'assets/images/SpaghettiCarbonara.jpeg', category: 'foods' },
-    { id: 3, name: 'Tiramisu', price: 5.99, description: 'Coffee-flavoured Italian dessert', image: 'assets/images/Tiramisu.jpg', category: 'foods' },
-    { id: 4, name: 'Cocktail', price: 6.99, description: 'Refreshing cocktail with fruits', image: 'assets/images/cocktail.jpg', category: 'drinks' },
-    { id: 5, name: 'Latte', price: 4.99, description: 'Creamy latte coffee', image: 'assets/images/latte.jpg', category: 'drinks' },
-    { id: 6, name: 'Smoothie', price: 5.99, description: 'Healthy fruit smoothie', image: 'assets/images/smoothie.jpg', category: 'drinks' }
-  ];
+    { id: 1, name: 'Pizza Margherita', price: 8.99, priceDiscount: 0, description: 'Classic pizza with tomatoes and mozzarella', image: 'assets/images/pizza.jpg', category: 'foods', hasDiscount: false },
+    { id: 2, name: 'Spaghetti Carbonara', price: 10.99, priceDiscount: 0, description: 'Pasta with eggs, cheese, pancetta, and pepper', image: 'assets/images/SpaghettiCarbonara.jpeg', category: 'foods', hasDiscount: false },
+    { id: 3, name: 'Tiramisu', price: 5.99, priceDiscount: 0, description: 'Coffee-flavoured Italian dessert', image: 'assets/images/Tiramisu.jpg', category: 'foods', hasDiscount: false },
+    { id: 4, name: 'Vodka absolut 4 CL', price: 17, priceDiscount: 12.99, description: 'Une vodka haut de gamme, produite à partir de blé dhiver suédois et deau de source. Elle est souvent consommée pure, avec des glaçons ou en cocktail.', image: 'assets/images/vodka.jpeg', category: 'drinks', hasDiscount: true },
+    { id: 5, name: 'Martini Rouge 5 CL', price: 10, priceDiscount: 0, description: 'un vermouth rouge, plus sucré que le blanc, avec une saveur légèrement amère.', image: 'assets/images/martiniRouge.jpeg', category: 'drinks', hasDiscount: false },
+    { id: 6, name: 'Johnnie Walker 4 CL', price: 26, priceDiscount: 19.99, description: 'Johnnie Walker Black Label 4cl', image: 'assets/images/jhonnyWalker.jpeg', category: 'drinks', hasDiscount: true }
+ ];
 
   constructor() { }
 
